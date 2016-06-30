1768b41e64532980e26c3b4c850b82c1ac3d9b17
Colin Edwards <colin@recursivepenguin.com>
2016-06-30 14:39:11 -0500

    Start simple CLI app

---
+++ spotifycmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/DDRBoxman/spotifycontrol"
+
+func main() {
+	spotifycontrol.NextTrack()
+}
--- spotifycontrol.go
+++ spotifycontrol.go
@@ -5,5 +5,20 @@ package spotifycontrol
 #cgo LDFLAGS: -framework ScriptingBridge -framework Foundation
 #import <Foundation/Foundation.h>
 #import "Spotify.h"
+
+SpotifyApplication*
+spotify(void) {
+	return [SBApplication applicationWithBundleIdentifier:@"com.spotify.client"];
+}
+
+void
+nextTrack(void) {
+	SpotifyApplication* app = spotify();
+	[app nextTrack];	
+}
 */
 import "C"
+
+func NextTrack() {
+	C.nextTrack()
+}
